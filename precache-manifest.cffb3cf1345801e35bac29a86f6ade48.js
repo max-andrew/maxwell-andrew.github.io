7a04577cb47bc99cf05c1e3e3de80fb1897c577e
maxwell-andrew <andrewmr@bc.edu>
2019-04-24 15:57:01 -0700

    Updates

---
+++ precache-manifest.cffb3cf1345801e35bac29a86f6ade48.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc9eebda2251075198d02eb66a9ab09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e294f10581a021b43d",
+    "url": "/static/js/main.f660448d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/maxwell-andrew/maxwell-andrew.github.io/precache-manifest.394ee2160f969db5983e01b4052b9b5c.js"
+  "/precache-manifest.cffb3cf1345801e35bac29a86f6ade48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maxwell-andrew/maxwell-andrew.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
