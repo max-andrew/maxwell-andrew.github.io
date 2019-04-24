fbeea93a0d0d500a5002a0e8569731e903343b1e
maxwell-andrew <andrewmr@bc.edu>
2019-04-24 14:43:48 -0700

    New

---
+++ precache-manifest.394ee2160f969db5983e01b4052b9b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6733b5f249e52a73921f26012d3ef3a8",
+    "url": "/maxwell-andrew/maxwell-andrew.github.io/index.html"
+  },
+  {
+    "revision": "f382fe2adfd754e15e01",
+    "url": "/maxwell-andrew/maxwell-andrew.github.io/static/css/main.60e32508.chunk.css"
+  },
+  {
+    "revision": "47aa93bac6fb46e15d4e",
+    "url": "/maxwell-andrew/maxwell-andrew.github.io/static/js/2.afee2e22.chunk.js"
+  },
+  {
+    "revision": "f382fe2adfd754e15e01",
+    "url": "/maxwell-andrew/maxwell-andrew.github.io/static/js/main.73c8ee88.chunk.js"
+  },
+  {
+    "revision": "4f7a16d172d00ca4111a",
+    "url": "/maxwell-andrew/maxwell-andrew.github.io/static/js/runtime~main.451bcb41.js"
+  },
+  {
+    "revision": "cc71a665a7b2b2d4233dac04d289cfd4",
+    "url": "/maxwell-andrew/maxwell-andrew.github.io/static/media/scientist.cc71a665.jpeg"
+  }
+]);
